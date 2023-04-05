80d6574c6789c1f6dc0452ea4e4a3d595bc5b0a9
sagar-gohel <sagar.180180107011@gmail.com>
2023-04-05 18:39:25 +0530

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  root: true,
+  extends: '@react-native-community',
+  rules: {
+    semi: ['error', 'always'],
+    quotes: ['error', 'single'],
+    beforeEach: true,
+    'eslint block-spacing': 'error',
+  },
+};

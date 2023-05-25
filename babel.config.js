d14d2dbcd25dd19bba16dc0779da63367877d67b
sagar-gohel <sagar.180180107011@gmail.com>
2023-05-25 11:04:48 +0530

    web-update

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,14 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  plugins: ['react-native-reanimated/plugin'],
+  plugins: ['react-native-reanimated/plugin', 'react-native-web'],
   env: {
     production: {
       plugins: ['react-native-paper/babel', 'react-native-reanimated/plugin'],
+    },
+  },
+  resolve: {
+    alias: {
+      'react-native$': 'react-native-web',
     },
   },
 };
